e4d6b39de4750d06bbf25dfa27269a93fa757d8e
Robert Hedges <hedges@ieee.org>
2014-08-27 17:12:22 -0500

    Initial check-in of True modified files.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -7,18 +7,18 @@ dpkg --get-selections | grep -v deinstall
 # Remove some packages to get a minimal install
 echo "==> Removing all linux kernels except the currrent one"
 dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v $(uname -r) | xargs apt-get -y purge
-echo "==> Removing linux source"
-dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
-echo "==> Removing development packages"
-dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
-echo "==> Removing documentation"
-dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
-echo "==> Removing development tools"
-apt-get -y purge build-essential
+#echo "==> Removing linux source"
+#dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+#echo "==> Removing development packages"
+#dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+#echo "==> Removing documentation"
+#dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
+#echo "==> Removing development tools"
+#apt-get -y purge build-essential
 echo "==> Removing default system Ruby"
 apt-get -y purge ruby ri doc
-echo "==> Removing default system Python"
-apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
+#echo "==> Removing default system Python"
+#apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
 echo "==> Removing X11 libraries"
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
 echo "==> Removing obsolete networking components"
@@ -38,38 +38,38 @@ while [ -n "$(deborphan --guess-all --libdevel)" ]; do
 done
 apt-get -y purge deborphan dialog
 
-echo "==> Removing man pages"
-rm -rf /usr/share/man/*
-echo "==> Removing APT files"
-find /var/lib/apt -type f | xargs rm -f
-echo "==> Removing anything in /usr/src"
-rm -rf /usr/src/*
-echo "==> Removing any docs"
-rm -rf /usr/share/doc/*
+#echo "==> Removing man pages"
+#rm -rf /usr/share/man/*
+#echo "==> Removing APT files"
+#find /var/lib/apt -type f | xargs rm -f
+#echo "==> Removing anything in /usr/src"
+#rm -rf /usr/src/*
+#echo "==> Removing any docs"
+#rm -rf /usr/share/doc/*
 echo "==> Removing caches"
 find /var/cache -type f -exec rm -rf {} \;
-# Cleanup Virtualbox
-rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
+## Cleanup Virtualbox
+#rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 
 # Remove unused locales
 rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt,pt_BR,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
 
-# Remove guest addition source
-rm -rf /usr/src/virtualbox-ose-guest*
-rm -rf /usr/src/vboxguest*
+## Remove guest addition source
+#rm -rf /usr/src/virtualbox-ose-guest*
+#rm -rf /usr/src/vboxguest*
 
-# Remove man pages
-rm -rf /usr/share/man/??
-rm -rf /usr/share/man/??_*
+## Remove man pages
+#rm -rf /usr/share/man/??
+#rm -rf /usr/share/man/??_*
 
 # Zero out the free space to save space in the final image:
 echo "Zeroing device to make space..."
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
-# Remove history file
-unset HISTFILE
-rm ~/.bash_history /home/vagrant/.bash_history
+## Remove history file
+#unset HISTFILE
+#rm ~/.bash_history /home/vagrant/.bash_history
 
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
